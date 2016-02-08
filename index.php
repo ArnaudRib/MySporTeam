a9c984fb8f0e7698cba485aa2c386e88bad4ba38
ArnaudRib <arnaud.ribeyrolles@isep.fr>
2016-02-08 18:31:17 +0100

    Rajout de commentaires :)

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+        <link rel="stylesheet" href="stylesheet.css"/>
         <title>MySporTeam</title>
         <meta charset="utf-8" />
     </head>
+
+
     <body>
+      <!--Menu en haut de la page-->
       <header>
         <div class="memeligne">
           <h4 style="display: inline-block; margin-left:30px;margin-right:30px; vertical-align: top;
@@ -22,14 +25,21 @@ padding: 7px 0px;">MySporTeam</h4>
           </ul>
         </div>
       </header>
+
+      <!--Contenu de la page-->
+      <div id="content"> 
         <h1 class="centre">Bienvenue</h1>
         <h2>Affichage de texte avec PHP</h2>
         <p>
             Cette ligne a été écrite entièrement en HTML.<br />
             <?php echo "Celle-ci a été écrite entièrement en PHP."; ?>
         </p>
-    </body>
-    <footer>
-      <p class="centre">Ceci est un test.</p>
-    </footer>
+      </div>
+
+      <!--Footer de la page-->
+      <footer>
+        <p class="centre">Ceci est un test.</p>
+      </footer>
+  </body>
+
 </html>
