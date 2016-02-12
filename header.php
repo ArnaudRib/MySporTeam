f40b90bc93c513b175f80af22b68a04e258866cd
tguigon <thomas@MacBook-Air-de-Thomas.local>
2016-02-12 17:42:04 +0100

    inscription

--- header.php
+++ header.php
@@ -3,12 +3,13 @@
     <h4 style="display: inline-block; margin-left:30px;margin-right:30px; vertical-align: top;
 padding: 5px 0px; font-size:20px; font-family:Arial;">MySporTeam</h4>
     <ul class="menu">
-      <li>Accueil</li>
+      <li> <a href="index.php"> Accueil</li>
       <li>MySporTeam</li>
       <li>Cours</li>
       <li>Training</li>
       <li>Compétitions</li>
       <li>Forums</li>
+      <li> <a href="Inscription.php">Inscription </a></li>
     </ul>
   </div>
 </header>
