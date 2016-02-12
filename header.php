be00f7cb41b4d40135d1bec2919682deb7f8dda2
Giordan Alexis <alexis.giordan@isep.fr>
2016-02-12 17:22:28 +0100

    création du header.php et changement dans l'index

---
+++ header.php
@@ -0,0 +1,14 @@
+<header>
+  <div id="hautdepage" class="memeligne">
+    <h4 style="display: inline-block; margin-left:30px;margin-right:30px; vertical-align: top;
+padding: 5px 0px; font-size:20px; font-family:Arial;">MySporTeam</h4>
+    <ul class="menu">
+      <li>Accueil</li>
+      <li>MySporTeam</li>
+      <li>Cours</li>
+      <li>Training</li>
+      <li>Compétitions</li>
+      <li>Forums</li>
+    </ul>
+  </div>
+</header>
--- index.php
+++ index.php
@@ -11,20 +11,7 @@
 
     <body>
       <!--Menu en haut de la page-->
-      <header>
-        <div id="hautdepage" class="memeligne">
-          <h4 style="display: inline-block; margin-left:30px;margin-right:30px; vertical-align: top;
-padding: 5px 0px; font-size:20px; font-family:Arial;">MySporTeam</h4>
-          <ul class="menu">
-            <li>Accueil</li>
-            <li>MySporTeam</li>
-            <li>Cours</li>
-            <li>Training</li>
-            <li>Compétitions</li>
-            <li>Forums</li>
-          </ul>
-        </div>
-      </header>
+      <?php include("header.php"); ?>
 
       <!--Contenu de la page-->
       <div id="content">
