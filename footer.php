6c2312eaff7c3bd55e7b35211fc9091988926155
Giordan Alexis <alexis.giordan@isep.fr>
2016-02-12 17:41:01 +0100

    ajouter du footer.php

---
+++ footer.php
@@ -0,0 +1,3 @@
+<footer>
+  <p class="centre" style="padding:10px;">Tout droits réservés, mySporteam<span style="font-size:13px;"><sup>TM</sup></span>.</p>
+</footer>
--- index.php
+++ index.php
@@ -14,50 +14,11 @@
       <?php include("header.php"); ?>
 
       <!--Contenu de la page-->
-      <div id="content">
-        <h1 class="centre">Bienvenue</h1>
-        <!--Partie Photographie-->
-        <section>
-          <div class="ligne1">
-            <div class="div1 usualbackground">
-              <span class="Police1">test</span>
-              <div class="img1 usualbackground" style="background-image:url('images/sport.png');"></div>
-
-            </div><div class="div1 usualbackground" >
-              <span class="Police1">test</span>
-              <div class="img1 usualbackground" style="background-image:url('images/sport2.jpg');"></div>
-            </div>
-          </div>
-          <div class="ligne2">
-            <div class="div1 usualbackground">
-              <span class="Police1">test</span>
-              <div class="img1 usualbackground" style="background-image:url('images/sport3.jpg');"></div>
-            </div><div class="div1 usualbackground">
-              <span class="Police1">test</span>
-              <div class="img1 usualbackground" style="background-image:url('images/sport4.jpg');"></div>
-            </div>
-          </div>
-        </section>
-
-        <!--Partie Texte-->
-        <aside>
-          <div class="div2 usualbackground">
-            <div class="img2 usualbackground" style="background-image:url('http://g02.a.alicdn.com/kf/HTB1flHVIpXXXXa1XFXXq6xXFXXXU/Automne-et-hiver-bande-verticale-Pyjamas-femmes-femmes-douces-sport-100-coton-des-v&ecirc;tements-de-nuit.jpg');"></div>
-              <div class="div2bis">
-                <div class="Haut1">
-                <p>test</p>
-                </div><div class="Bas1">
-                  <p>test</p>
-                </div>
-            </div>
-          </div>
-        </aside>
-      </div>
-
+      <?php include("nav.php"); ?>
+      
       <!--Footer de la page-->
-      <footer>
-        <p class="centre" style="padding:10px;">Tout droits réservés, mySporteam<span style="font-size:13px;"><sup>TM</sup></span>.</p>
-      </footer>
+      <?php include("footer.php"); ?>
+
   </body>
 
 </html>
