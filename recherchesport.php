2fd73746d364842e7565b51b72f6337c62220a9d
ArnaudRib <arnaud.ribeyrolles@isep.fr>
2016-02-18 01:44:11 +0100

    Commentaires -.-... bonne nuit :c

--- recherchesport.php
+++ recherchesport.php
@@ -9,19 +9,19 @@ $password = "";
 // Nom de la base de donnée
 $dbname = "MySporTeamBDD";
 
-$co = mysqli_connect($servername, $username, $password, $dbname);
+$co = mysqli_connect($servername, $username, $password, $dbname); // se connecter à la bdd
 
 $text = $_GET['resultat'];
 
 $sql = "SELECT id, nom, photo FROM Sports WHERE nom LIKE '%$text%' LIMIT 10";
 $result = mysqli_query($co, $sql);
 
-if (mysqli_num_rows($result) == 0) {?>
+if (mysqli_num_rows($result) == 0) { //Si la recherche ne donne rien?>
   <p style="color:red;"> <?php echo "Désolé :( </br> YA PAS DE SPORT QUI S'APPELLE $text !!!";?> </p>
 <?php
 }
 
-while ($row = mysqli_fetch_assoc($result)) {
+while ($row = mysqli_fetch_assoc($result)) { //row choisit une seule ligne.
   ?>
     <div title="<?php echo $row['nom']?>" class="boxes" style="background-image: url('<?php echo $row['photo']?>')";></div>
   <?php
