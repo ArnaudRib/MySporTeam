324e8ebe872ffc501817275700533a9156670109
ArnaudRib <arnaud.ribeyrolles@isep.fr>
2016-03-11 14:20:41 +0100

    Mise en ligne.

--- connection.php
+++ connection.php
@@ -12,20 +12,27 @@
     <section class="bloc1">
       <div class="encadrement" style="padding:3%;">
         <h2>Déjà inscrit?</h2>
-        <form style="margin:10px;" action="verification.php" method="post">
-          <fieldset>
-            Adresse Email :
-            <input type="text" name="email" placeholder="Email" required> </br>
 
-            Mot de passe :
-            <input type="password" name="mot_de_passe" placeholder="Mot de passe" required> </br>
+        <?php
+        if ($id!=0) erreur(ERR_IS_CO);
+        ?>
+        <?php
+        if (!isset($_POST['pseudo'])){ //On est dans la page de formulaire ?>
+          <form style="margin:10px;" action="verification.php" method="post">
+            <fieldset>
+              <label for="pseudo">Adresse Email :</label>
+              <input type="text" name="pseudo" placeholder="Email" required> </br>
 
-            <a href="#tamere">Mot de passe oublié?</a></br>
-            <input type="submit" name="Envoyer"> <br>
+              <label for="motdepasse">Mot de passe :</label>
+              <input type="password" name="mot_de_passe" placeholder="Mot de passe" required> </br>
 
-          </fieldset>
+              <a href="#tamere">Mot de passe oublié?</a></br>
+              <input type="submit" name="Envoyer"> <br>
+            </fieldset>
+          </form>
+        <?php
+      }?>
 
-        </form>
       </div>
     </section>
 
