7e67f4e5e561099efe4a9c837be2b50abda07bab
AlexandreMaistret <alexandre.maistret@isep.fr>
2016-03-14 14:06:28 +0100

    changement header

--- header.php
+++ header.php
@@ -1,10 +1,17 @@
 <header>
-  <div id="stylesheet.css" class="memeligne">
-    <ul class="menu">
-      <li> <a href= "index.php"> <img src="Images/mysporteam.png" width="285px" height="200px"></a></li>
-      <li> <img src= "user.png"></li>
-      <li> <a href="connexion.php">Connexion </a></li>
-      <li> <a href="Inscription.php">Inscription </a></li>
-    </ul>
-  </div>
+  <div class=logo>
+    <a href="">
+    <img id=logo src=Images/mysporteam2.png  width=15% />
+  </a>
+</div>
+
+
+<div class=menu>
+  <ul>
+    <li><input type="searchbar" placeholder="Entrez votre recherche" name="Recherche"/>
+    <li> <a id="bouton" href="forums.php">Forums </a></li>
+    <li> <a id="bouton" href="connexion.php">Connexion </a></li>
+    <li> <a id="bouton" href="Inscription.php">Inscription </a></li>
+  </ul>
+</div>
 </header>
