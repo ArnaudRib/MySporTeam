1c4f75d30bbdad6deb0e30f4887146ff3d7159e6
AlexandreMaistret <alexandre.maistret@isep.fr>
2016-03-22 00:53:18 +0100

    Header + page groupe

--- footer.php
+++ footer.php
@@ -1,3 +1,9 @@
 <footer>
-  <p class="centre" style="padding:10px;">Tout droits réservés, mySporteam<span style="font-size:13px;"><sup>TM</sup></span>.</p>
+    <div class="textfooter">
+        <ul>
+        <li> <a  href=""><img src="https://www.facebook.com/images/fb_icon_325x325.png" width="35px"/> </a></li>
+        <li><p style="padding:10px;">Tout droits réservés, mySporteam<span style="font-size:13px;"><sup>TM</sup></span>.</p></li>
+      </ul>
+    </div>
+
 </footer>
