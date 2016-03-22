831b7d8411ba003f0f54a2fc2dfe6ea84f2fce99
AdrienCeron <adrien.ceron@yahoo.fr>
2016-03-22 11:59:46 +0100

    debut page recherchegroupe

---
+++ RechercheGroupe.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="stylesheet.css"/>
+    <title>MySporTeam</title>
+    <meta charset="utf-8" />
+  <head>
+
+  <body>
+      <?php include("header.php"); ?>
+
+      <div class="groupe">
+        <div class="imagegroupe usualbackground" style="background-image:url(images/sport.png);"> </div>
+        <div class="nomgroupe">
+          <h3> Nom groupe </h3>
+          <p>Test </p>
+        </div>
+        <div id="descriptiongroupe"> Homines enim eruditos et sobrios ut infaustos et inutiles vitant, eo quoque accedente quod et nomenclatores adsueti haec et talia venditare, mercede accepta lucris quosdam et prandiis inserunt subditicios ignobiles et obscuros.
+        </div>
+      </div>
+
+      <?php include("footer.php"); ?>
+  <body>
