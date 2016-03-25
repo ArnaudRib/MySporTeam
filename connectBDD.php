9fc6ffb0cb9eca5af73c65adbfce4a01f18c5fa2
ArnaudRib <arnaud.ribeyrolles@isep.fr>
2016-03-25 11:30:39 +0100

    Fix des conflits bd

--- connectBDD.php
+++ connectBDD.php
@@ -14,20 +14,3 @@ $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 // set the PDO error mode to exception
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
-=======
-<?php
-
-// url du serveur
-$servername = "localhost";
-// Nom d'utilisateur Base de données
-$username = "root";
-// MDP utilisateur
-$password = "root";
-// Nom de la base de donnée
-$dbname = "MySporTeamBDD";
-
-$db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-// set the PDO error mode to exception
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
->>>>>>> Stashed changes
