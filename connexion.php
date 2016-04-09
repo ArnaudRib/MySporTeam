761e3d377b71c04644bba445ef06a734554c9aa0
Giordan Alexis <alexis.giordan@isep.fr>
2016-04-09 22:27:39 +0200

    changement de la page connexion et inscription

--- connexion.php
+++ connexion.php
@@ -1,79 +1,118 @@
 <?php session_start(); ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <link rel="stylesheet" href="stylesheet.css">
+  <link rel="stylesheet" href="stylesheet.css"/>
+  <link rel="stylesheet" href="inscription.css"/>
   <title>MySporTeam</title>
+  <meta charset="utf-8" />
 </head>
+
+
 <body>
+  <!--Menu en haut de la page-->
   <?php include("header.php"); ?>
-  <h1 class="">Connexion</h1>
 
-  <section class="bloc1">
-    <div class="encadrement" style="padding:3%;">
-      <h2>Déjà inscrit?</h2>
-      <?php
-      if (!isset($_POST['pseudo'])){ //On est dans la page de formulaire ?>
-        <form style="margin:10px;" action="connexion.php" method="post">
-          <fieldset>
-            <label for="pseudo">Pseudo :</label>
-            <input type="text" name="pseudo" placeholder="Pseudo" required> </br>
+  <!--Contenu de la page-->
 
-            <label for="motdepasse">Mot de passe :</label>
-            <input type="password" name="mot_de_passe" placeholder="Mot de passe" required> </br>
+  <div id="inscription">
+    <form action="connexion.php" method="post">
 
-            <a href="#tamere">Mot de passe oublié?</a></br>
-            <input type="submit" name="Envoyer"> <br>
-          </fieldset>
-        </form>
-        <?php
-      }else{
-        $message='';
-        if (empty($_POST['pseudo']) || empty($_POST['mot_de_passe']) ) //Oublie d'un champ
-        {
-          $message = '<p>une erreur s\'est produite pendant votre identification.
-          Vous devez remplir tous les champs</p>
-          <p>Cliquez <a href="./connexion.php">ici</a> pour revenir</p>';
-        } else {//On check le mot de passe
-          include('connectBDD.php');
-          $sql="SELECT * FROM utilisateurs WHERE pseudo=? AND mot_de_passe=?";
-          $query=$db->prepare($sql); //a mettre dans model
-          $query->execute([$_POST['pseudo'], sha1($_POST['mot_de_passe'])]);
-          $data=$query->fetch();
-
-          if ($data['mot_de_passe'] == sha1($_POST['mot_de_passe'])) // Acces OK !
-          {
-            $_SESSION['pseudo'] = $data['pseudo'];
-            $_SESSION['mot_de_passe'] = $data['mot_de_passe'];
-            $message = '<p>Bienvenue '.$data['pseudo'].',
-            vous êtes maintenant connecté!</p>
-            <p>Cliquez <a href="./index.php">ici</a>
-            pour revenir à la page d accueil</p>';
-          }
-          else // Acces pas OK !
-          {
-            $message = '<p>Une erreur s\'est produite
-            pendant votre identification.<br /> Le mot de passe ou le pseudo
-            entré n\'est pas correcte.</p><p>Cliquez <a href="./connexion.php">ici</a>
-            pour revenir à la page précédente
-            <br /><br />Cliquez <a href="./index.php">ici</a>
-            pour revenir à la page d accueil</p>';
-          }
-          $query->CloseCursor();
-        }
-        echo $message.'</div></body></html>';
-      }
-      ?>
-    </div>
-  </section>
-
-  <aside class="bloc2">
-    <div class="encadrement" style="padding:3%;">
-      <h2>Pas encore inscrit?</h2>
-      <a href="inscription.php">ICI<a>
+      <div class="haut_inscription">
+        <h1>Connexion</h1>
       </div>
-    </aside>
-    <?php include("footer.php"); ?>
-  </body>
-  </html>
+
+
+      <fieldset>
+        <?php
+        if(isset($_POST['pseudo'])) {
+          $message='';
+
+          if (empty($_POST['pseudo']) || empty($_POST['mot_de_passe'])) { //Oublie d'un champ
+            echo '<p class="error_connexion">Vous devez remplir tous les champs.</p>'; ?>
+            <br />
+            <div>
+              <label for="login"><img src="images/icone_utilisateur.png" /></label>
+              <input type="text" name="pseudo" placeholder="Email ou Pseudo" required>
+            </div> <br />
+
+              <div><label for="mdp"><img src="images/icone_lock.png" /></label>
+                <input id="mdp" type="password" name="mot_de_passe" placeholder="Mot de passe" oninput="Verification()" required></div> <br />
+
+                <p><a href=""> Mot de passe oublié ? </a>|<a href="inscription.php"> Pas inscrit ?</a></p>
+
+                <br />
+                <input id="submit" type="submit" name="Envoyer" value="Se connecter"> <br />
+
+              </fieldset>
+          <?php
+        }else {//On check le mot de passe
+
+            include('connectBDD.php');
+
+            $sql="SELECT * FROM utilisateurs WHERE pseudo=? AND mot_de_passe=?";
+            $query=$db->prepare($sql); //a mettre dans model
+            $query->execute(array($_POST['pseudo'], sha1($_POST['mot_de_passe'])));
+            $data=$query->fetch();
+
+            if ($data['mot_de_passe'] != sha1($_POST['mot_de_passe'])) {// Acces OK !
+              echo '<p class="error_connexion">Une erreur s\'est produite
+              pendant votre identification.<br /> Le mot de passe ou l\'identifiant
+              entré n\'est pas correcte.</p>';
+              ?>
+
+              <br />
+              <div>
+                <label for="login"><img src="images/icone_utilisateur.png" /></label>
+                <input type="text" name="pseudo" placeholder="Email ou Pseudo" required>
+              </div> <br />
+
+                <div><label for="mdp"><img src="images/icone_lock.png" /></label>
+                  <input id="mdp" type="password" name="mot_de_passe" placeholder="Mot de passe" oninput="Verification()" required></div> <br />
+
+                  <p><a href=""> Mot de passe oublié ? </a>|<a href="inscription.php"> Pas inscrit ?</a></p>
+
+                  <br />
+                  <input id="submit" type="submit" name="Envoyer" value="Se connecter"> <br />
+
+                </fieldset>
+
+                <?php
+              }else {// Acces pas OK !
+                $_SESSION['pseudo'] = $data['pseudo'];
+                $_SESSION['mot_de_passe'] = $data['mot_de_passe'];
+                echo '<p class="msg_connexion">Bonjour '.$data['pseudo'].',
+                vous êtes maintenant connecté!</p>
+                <p>Cliquez <a href="./index.php">ici</a>
+                pour revenir à la page d accueil</p>';
+              }
+              $query->CloseCursor();
+            }
+          } else {
+          ?>
+          <br />
+          <div>
+            <label for="login"><img src="images/icone_utilisateur.png" /></label>
+            <input type="text" name="pseudo" placeholder="Email ou Pseudo" required>
+          </div> <br />
+
+            <div><label for="mdp"><img src="images/icone_lock.png" /></label>
+              <input id="mdp" type="password" name="mot_de_passe" placeholder="Mot de passe" oninput="Verification()" required></div> <br />
+
+              <p><a href=""> Mot de passe oublié ? </a>|<a href="inscription.php"> Pas inscrit ?</a></p>
+
+              <br />
+              <input id="submit" type="submit" name="Envoyer" value="Se connecter"> <br />
+
+            </fieldset>
+            <?php } ?>
+        </form>
+      </div>
+
+      <!--Footer de la page-->
+      <?php include("footer.php"); ?>
+      <script src="Verification.js"></script>
+    </body>
+
+    </html>
--- formulaire.php
+++ formulaire.php
@@ -1,8 +1,8 @@
 <?php
 include('connectBDD.php');
+
 $password=sha1($_POST['mot_de_passe']);
-$date_de_naissance=$_POST['annee']."-".$_POST['mois']."-".$_POST['jour'];
-$query=$db->prepare("INSERT INTO utilisateurs (pseudo,mot_de_passe,sexe,email,naissance) VALUES (?,?,?,?,?)");
-$query->execute(array($_POST['pseudo'],$password,$_POST['sexe'],$_POST['email'],$date_de_naissance));
+$query=$db->prepare("INSERT INTO utilisateurs (pseudo,mot_de_passe,email) VALUES (?,?,?)");
+$query->execute(array($_POST['pseudo'],$password,$_POST['email']));
 header("Location: index.php");
 ?>
