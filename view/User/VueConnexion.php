b724f23ea69432bc6e26f440365861da8fe30611
ArnaudRib <arnaud.ribeyrolles@isep.fr>
2016-05-11 10:02:40 +0200

    fix background des pages ,_,

--- view/User/VueConnexion.php
+++ view/User/VueConnexion.php
@@ -5,27 +5,31 @@
   header('Location: index.php'); //A changer :(..
 endif; */?>
 
-<div id="inscription">
-  <form action="" method="post">
+<div class="bodybackground">
+  <div class="blockinscription">
+    <div id="inscription">
+      <form action="" method="post">
 
-    <div class="haut_inscription">
-      <h1>Connexion</h1>
+        <div class="haut_inscription">
+          <h1>Connexion</h1>
+        </div>
+
+        <fieldset>
+          <div>
+            <label for="login"><img src="asset/images/Users/icone_utilisateur.png" /></label>
+            <input type="text" name="pseudo" placeholder="Email ou Pseudo" required>
+          </div> <br />
+
+          <div><label for="mdp"><img src="asset/images/Users/icone_lock.png" /></label>
+            <input id="mdp" type="password" name="mot_de_passe" placeholder="Mot de passe" oninput="Verification()" required></div> <br />
+            <p><a href=""> Mot de passe oublié ? </a>|<a href="inscription"> Pas inscrit ?</a></p>
+
+            <?php echo $message; ?>
+          </br>
+          <input id="submit" type="submit" name="Envoyer" value="Se connecter"> <br />
+
+        </fieldset>
+      </form>
     </div>
-
-    <fieldset>
-      <div>
-        <label for="login"><img src="asset/images/Users/icone_utilisateur.png" /></label>
-        <input type="text" name="pseudo" placeholder="Email ou Pseudo" required>
-      </div> <br />
-
-      <div><label for="mdp"><img src="asset/images/Users/icone_lock.png" /></label>
-        <input id="mdp" type="password" name="mot_de_passe" placeholder="Mot de passe" oninput="Verification()" required></div> <br />
-        <p><a href=""> Mot de passe oublié ? </a>|<a href="inscription"> Pas inscrit ?</a></p>
-
-        <?php echo $message; ?>
-      </br>
-        <input id="submit" type="submit" name="Envoyer" value="Se connecter"> <br />
-
-      </fieldset>
-    </form>
   </div>
+</div>
--- view/User/VueInscription.php
+++ view/User/VueInscription.php
@@ -1,31 +1,33 @@
+<div class="bodybackground">
+  <div class="blockinscription">
+    <nav id="inscription">
+      <form action="" method="post">
+        <div class="haut_inscription">
+          <h1>Inscription</h1>
+        </div>
 
-<nav id="inscription">
-  <form action="" method="post">
-    <div class="haut_inscription">
-      <h1>Inscription</h1>
-    </div>
+        <fieldset>
 
-    <fieldset>
+          <br />
+          <div><label for="pseudo"><img src="asset/images/Users/icone_utilisateur.png" /></label>
+          <input type="text" name="pseudo" placeholder="Identifiant" required></div> <br />
 
-      <br />
-      <div><label for="pseudo"><img src="asset/images/Users/icone_utilisateur.png" /></label>
-      <input type="text" name="pseudo" placeholder="Identifiant" required></div> <br />
+          <div><label for="email" class="email"><img src="asset/images/Users/icone_email.png" /></label>
+          <input type="text" name="email" placeholder="Email" required></div> <br />
 
-      <div><label for="email" class="email"><img src="asset/images/Users/icone_email.png" /></label>
-      <input type="text" name="email" placeholder="Email" required></div> <br />
+          <div><label for="password"><img src="asset/images/Users/icone_lock.png" /></label>
+          <input id="mdp" type="password" name="mot_de_passe" placeholder="Mot de passe" oninput="Verification()" required></div> <br />
 
-      <div><label for="password"><img src="asset/images/Users/icone_lock.png" /></label>
-      <input id="mdp" type="password" name="mot_de_passe" placeholder="Mot de passe" oninput="Verification()" required></div> <br />
+          <div><label for="check_psswd"><img src="asset/images/Users/icone_lock.png" /></label>
+          <input id="mdp_verification" type="password" name="mot_de_passe_confirmation" placeholder="Confirmation du mdp" oninput="Verification()" required></div> <br />
 
-      <div><label for="check_psswd"><img src="asset/images/Users/icone_lock.png" /></label>
-      <input id="mdp_verification" type="password" name="mot_de_passe_confirmation" placeholder="Confirmation du mdp" oninput="Verification()" required></div> <br />
-
-      <input id="submit" type="submit" name="Envoyer" value="S'inscrire"> <br />
-      <?php echo $message; ?>
-    </fieldset>
-  </form>
-</nav>
-
+          <input id="submit" type="submit" name="Envoyer" value="S'inscrire"> <br />
+          <?php echo $message; ?>
+        </fieldset>
+      </form>
+    </nav>
+  </div>
+</div>
 
 <!--
 ANCIEN CONTENU .. A NE PAS EFFACER, SERA UTILE POUR COMPLETER MON PROFIL PROBABLEMENT.
