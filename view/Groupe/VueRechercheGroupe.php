18a7fb2f2505412f464c321723eecd6189e4fcce
ArnaudRib <arnaud.ribeyrolles@isep.fr>
2016-05-12 02:57:24 +0200

    fix $i recherche ;_;

--- view/Groupe/VueRechercheGroupe.php
+++ view/Groupe/VueRechercheGroupe.php
@@ -16,9 +16,10 @@
           </div>
           <div class="niveau">
             <h3>Niveau</h2>
+              <?php $i=1; ?>
               <?php foreach ($niveau as $key => $value): ?>
                 <div class="eachniveau">
-                  <input type="radio" name="niveau" id="niveau<?php echo $i=count($niveau)?>" value="<?php echo $i?>">
+                  <input type="radio" name="niveau" id="niveau<?php echo $i ?>" value="<?php echo $i?>">
                   <label for="niveau<?php echo $i ?>"><?php echo $value['nom'] ?></label>
                 </div>
                 <?php $i+=1; ?>
