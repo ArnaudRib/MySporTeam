f84045136af6d6e50ff94854d08602bd871fc13b
ArnaudRib <arnaud.ribeyrolles@isep.fr>
2016-05-26 10:53:48 +0200

    fix

--- asset/js/aide.js
+++
@@ -1,5 +0,0 @@
-function popupshow(){
-
-var popup=document.getElementById("popup");
-popup.style.display="inline";
-}
--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once("config/Route.php");
-$route=new Route;
-$route->getPage();
